3300e11057f239fbad3ec970da54a3934174eb34
kihyuk.jeong <kihyuk.jeong@musinsa.com>
2023-09-12 20:40:18 +0900

    선착순 쿠폰 발급 - Redis incr

---
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -0,0 +1,39 @@
+package com.example.api.service;
+
+import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
+import com.example.api.repository.CouponRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplyService {
+
+    private final CouponRepository couponRepository;
+    private final CouponCountRepository couponCountRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+        this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
+    }
+
+    // 쿠폰 발급 로직
+    public void apply(Long userId) {
+
+        long count = couponRepository.count();
+
+        if (count > 100) {
+            return;
+        }
+        couponRepository.save(new Coupon(userId));
+    }
+
+    public void apply_redis(Long userId) {
+
+        long count = couponCountRepository.increment();
+
+        if (count > 100) {
+            return;
+        }
+        couponRepository.save(new Coupon(userId));
+    }
+}
---
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.api.service;
+
+import com.example.api.repository.CouponRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    @DisplayName("1번만 응모")
+    void apply() {
+        applyService.apply(1L);
+        assertEquals(1, couponRepository.count());
+    }
+
+    @Test
+    @DisplayName("동시에 여러번 응모 - race condition 발생")
+    void apply_concurrency() throws InterruptedException {
+
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+        assertThat(count).isNotEqualTo(100);
+
+    }
+}
